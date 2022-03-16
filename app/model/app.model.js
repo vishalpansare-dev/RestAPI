a633cae5c3364984e154c862ccbbbb5f84dbe051
vishalpansare-dev <70095753+vishalpansare-dev@users.noreply.github.com>
2022-03-16 19:45:35 +0530

    init

---
+++ app/model/app.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const AppSchema = mongoose.Schema({
+  title:  String, // String is shorthand for {type: String}
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+
+module.exports = mongoose.model("App", AppSchema);
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+ const mongoose = require("mongoose");
+
+ mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb+srv://root:root@cluster0.yismy.mongodb.net/database?retryWrites=true&w=majority", {
+  
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Error...", err);
+    process.exit();
+  });
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running :D" });
+});
+
+let PORT = 8080;
+
+require("./app/routes/app.routes.js")(app);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
